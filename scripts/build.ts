a3e0a343029fae97996cd46e5dbef91984e1bb49
lepzulnag <gindre.matthieu@gmail.com>
2022-01-05 17:36:15 +0100

    📦 better dependencies and build bumping

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,5 @@
 import { compile, patch } from "@digitak/tsc-esm"
-import { rmSync, cpSync } from "fs"
+import { rmSync, cpSync, readFileSync, writeFile, writeFileSync } from "fs"
 
 console.log("Cleaning package...")
 rmSync("package", { recursive: true, force: true })
@@ -7,9 +7,17 @@ rmSync("package", { recursive: true, force: true })
 console.log("Compiling typescript...")
 compile()
 
+console.log("Patching imports...")
+patch()
+
+console.log("Bump build version...")
+const packageInfos = JSON.parse(readFileSync("./package.json", "utf-8"))
+const [major, minor, build] = packageInfos.version
+	.split(".")
+	.map((value: string) => parseInt(value))
+packageInfos.version = [major, minor, build + 1].join(".")
+writeFileSync("./package.json", JSON.stringify(packageInfos, null, "\t"))
+
 console.log("Copying configuration files...")
 cpSync("./README.md", "./package/README.md")
 cpSync("./package.json", "./package/package.json")
-
-console.log("Patching imports...")
-patch()
