dc9790253ed1c84fe9f7fcaa841b7f455e3a3ea3
lepzulnag <gindre.matthieu@gmail.com>
2022-04-19 16:36:55 +0200

    👷 add CI

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,23 +1,14 @@
-import { compile, patch } from "@digitak/tsc-esm"
-import { rmSync, cpSync, readFileSync, writeFile, writeFileSync } from "fs"
+import { execSync } from "child_process"
+import * as fs from "fs"
 
 console.log("Cleaning package...")
-rmSync("package", { recursive: true, force: true })
+fs.rmSync("package", { force: true, recursive: true })
 
 console.log("Compiling typescript...")
-compile()
-
-console.log("Patching imports...")
-patch()
-
-console.log("Bump build version...")
-const packageInfos = JSON.parse(readFileSync("./package.json", "utf-8"))
-const [major, minor, build] = packageInfos.version
-	.split(".")
-	.map((value: string) => parseInt(value))
-packageInfos.version = [major, minor, build + 1].join(".")
-writeFileSync("./package.json", JSON.stringify(packageInfos, null, "\t"))
+execSync("tsc")
 
 console.log("Copying configuration files...")
-cpSync("./README.md", "./package/README.md")
-cpSync("./package.json", "./package/package.json")
+fs.copyFileSync("./README.md", "./package/README.md")
+fs.copyFileSync("./package.json", "./package/package.json")
+
+console.log("✨ Build done\n")
